386994b859faeb90580d3e89815b4cb05081e980
Ahmed Ali <ahmedalinicobar@gmail.com>
2024-03-27 19:39:34 +0500

    New Upload

--- code.py
+++
@@ -1,9 +0,0 @@
-fin = open("message.txt", "r").read()
-cipher = [pow(int(i), -1, 41) for i in fin.split()]
-for i in cipher:
-    if i < 27:
-        print(chr(i + 96), end="")
-    elif i < 37:
-        print(i - 27, end="")
-    else:
-        print("_", end="")
